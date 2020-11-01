84c0874e8c87ec9fbb82c41327f6551ea6809376
David Alami <david.alami@gmail.com>
2020-11-01 12:44:58 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,173 +1,109 @@
 #coding:utf-8
-import numpy as np
 import torch
-import GRCNN
-import dataloader
-from torch.autograd import Variable
-import torch.optim as optim
-import cv2
-import os
-from warpctc_pytorch import CTCLoss
-from glob import glob
-import utils
-import shutil
-from tqdm import tqdm
-import argparse
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-parser = argparse.ArgumentParser(description='PyTorch GCRNN Training')
-
-parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
-parser.add_argument('--beta1', default=0.5, type=float, help='beta1')
-parser.add_argument('--beta2', default=0.999, type=float, help='beta2')
-parser.add_argument('--epoches', default=2, type=int, help='the number of train')
-parser.add_argument('--gpu_list', default='-1', type=str, help='gpu_list')
-parser.add_argument('--batch_size', default=2, type=int, help='min batch size')
-parser.add_argument('--n_class', default=37, type=int, help='the number of class')
-parser.add_argument('--n_hidden', default=64, type=int, help='the number of hidden unit')
-parser.add_argument('--img_w', default=300, type=int, help='the width of image')
-parser.add_argument('--img_h', default=60, type=int, help='the height of image')
-parser.add_argument('--max_len', default=20, type=int, help='max length(for data load)')
-parser.add_argument('--num_worker', default=2, type=int, help='the number of work')
-parser.add_argument('--alphabet', default='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', type=str, help='alphabet')
-parser.add_argument('--model_save_path', default='./model_result/', type=str, help='model_save_path')
-parser.add_argument('--train_main_path', default='./data_sample/', type=str, help='train data path')
-parser.add_argument('--val_main_path', default='./data_sample/', type=str, help='validation set path')
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-args = parser.parse_args()
+class BidirectionalLSTM(nn.Module):
 
+    def __init__(self, nIn, nHidden, nOut):
+        super(BidirectionalLSTM, self).__init__()
 
-if os.path.exists(args.model_save_path):
-    shutil.rmtree(args.model_save_path)
-os.mkdir(args.model_save_path)
+        self.rnn = nn.LSTM(nIn, nHidden, bidirectional=True)
+        self.embedding = nn.Linear(nHidden * 2, nOut)
 
-dataset = dataloader.MyDataSet(args.train_main_path, width=args.img_w, height=args.img_h, max_len = args.max_len)
-trainloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_worker)
-val_img_list = glob(args.val_main_path + '*.jpg')
+    def forward(self, input):
+        recurrent, _ = self.rnn(input)
+        T, b, h = recurrent.size()
+        t_rec = recurrent.view(T * b, h)
 
-# custom weights initialization called on crnn
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(0)
-crnn = GRCNN.GRCNN(args.n_class)
-crnn.apply(weights_init)
+        output = self.embedding(t_rec)  # [T * b, nOut]
+        output = output.view(T, b, -1)
+
+        return output
+
+class GRCL(nn.Module):
+    def __init__(self, in_channels, out_channels, n_iter = 3, kernel_size=3, padding=(1, 1), stride=(1, 1)):
+        super(GRCL, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.n_iter = n_iter
+
+        self.conv_r = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride)
+        self.conv_g_r = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1)
+
+        self.conv_f = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+                                    nn.BatchNorm2d(out_channels))
+
+        self.conv_g_f = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+                                         nn.BatchNorm2d(out_channels))
+
+        self.bn_rec = nn.ModuleList()
+        self.bn_gate_rec = nn.ModuleList()
+        self.bn_gate_mul = nn.ModuleList()
+        for ii in range(n_iter):
+            self.bn_rec.append(nn.BatchNorm2d(out_channels))
+            self.bn_gate_rec.append(nn.BatchNorm2d(out_channels))
+            self.bn_gate_mul.append(nn.BatchNorm2d(out_channels))
 
 
 
-crnn = crnn.to(device)
-criterion = CTCLoss().to(device)
+    def forward(self, x):
+        conv_gate_f = self.conv_g_f(x)
+        bn_f = self.conv_f(x)
+        x = F.relu(bn_f)
 
-#net.cuda()
-print('net has load!')
-converter = utils.strLabelConverter(args.alphabet)
+        for ii in range(self.n_iter):
+            c_gate_rec = self.bn_gate_rec[ii](self.conv_g_r(x))
+            gate = F.sigmoid(conv_gate_f + c_gate_rec)
 
-optimizer=optim.Adam(crnn.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
+            c_rec = self.bn_rec[ii](self.conv_r(x))
+            x = F.relu(bn_f + self.bn_gate_mul[ii](c_rec*gate))
 
-best_acc=-1
-totalLoss=[]
-avg_test_acc = []
-avg_train_acc = []
+        return x
 
-def get_img(img_path):
-    img = cv2.imread(img_path)
-    img= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img, (args.img_w, args.img_h))
-    img = np.reshape(img, newshape=[1,1, args.img_h, args.img_w])
-    img = img.astype(np.float32)
-    img = img / 255
-    img = img - 0.5
-    img = img * 2
-    img_tensor = torch.from_numpy(img).float().to(device)
-    true_label = img_path.replace('.jpg', '').split('_')[-1]
-    return img_tensor, true_label
+class GRCNN(nn.Module):
+    def __init__(self, n_class=37):
+        super(GRCNN, self).__init__()
+        self.n_class = n_class
+        self.conv_layer_1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
+                                          nn.BatchNorm2d(64), nn.ReLU())
+        self.GRCL_layer_1 = GRCL(64, 64, kernel_size=3, stride=(1, 1), padding=(1, 1))
+        self.GRCL_layer_2 = GRCL(64, 128, kernel_size=3, stride=(1, 1), padding=(1, 1))
+        self.GRCL_layer_3 = GRCL(128, 256, kernel_size=3, stride=(1, 1), padding=(1, 1))
 
+        self.conv_layer_2 = nn.Sequential(nn.Conv2d(256, 512, kernel_size=2, stride=1, padding=0),
+                                          nn.BatchNorm2d(512), nn.ReLU())
+        self.rnn = nn.Sequential(
+            BidirectionalLSTM(512, 256, 256),
+            BidirectionalLSTM(256, 256, self.n_class))
 
-for epoch in range(1, args.epoches, 1):
-    #train
-    for p in crnn.parameters():
-        p.requires_grad = True
-    crnn.train()
-    avg_totalLoss=0.0
-    train_acc = 0.0
-    test_acc = 0.0
-    for batch_id,(img_tensor, txt_len, txt_label, txt_name) in tqdm(enumerate(trainloader)):
-        optimizer.zero_grad()
-        batch_length = img_tensor.size(0)
-        txt_label = txt_label.numpy().reshape(args.max_len*batch_length)
-        txt_label = torch.from_numpy(np.array([item for item in txt_label if item != 0]).astype(np.int))
+    def forward(self, x):
+        x = self.conv_layer_1(x)
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        x = self.GRCL_layer_1(x)
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        x = self.GRCL_layer_2(x)
+        x = F.max_pool2d(x, kernel_size=2, stride=(2, 1), padding=(0, 1))
+        x = self.GRCL_layer_3(x)
+        x = F.max_pool2d(x, kernel_size=2, stride=(2, 1), padding=(0, 1))
+        conv = self.conv_layer_2(x)
 
-        img_tensor = Variable(img_tensor.float()).to(device)
-
-        txt_len = Variable(txt_len.int()).squeeze(1)
-        txt_label = Variable(txt_label.int())
-
-        preds = crnn(img_tensor)
-        preds_size = Variable(torch.IntTensor([preds.size(0)] * batch_length))
-        total_loss = criterion(preds, txt_label, preds_size, txt_len) / batch_length
-        total_loss.backward()
-        optimizer.step()
-        _, preds = preds.max(2)
-        preds = preds.transpose(1, 0).contiguous().view(-1)
-        sim_preds = converter.decode(preds.data, preds_size.data, raw=False)
-        for pred, target in zip(sim_preds, txt_name):
-            if pred == target:
-                train_acc += 1
-        avg_totalLoss += total_loss.item()
-        info='epoch : %d ,process: %d/%d ,  totalLoss: %f , lr: %f  ' % (epoch, batch_id, trainloader.__len__(), total_loss.item(), optimizer.param_groups[0]['lr'])
-        #print(info)
-        #break
-    train_acc /= len(dataset)
-    avg_train_acc.append(train_acc)
-
-    #valid
-    for p in crnn.parameters():
-        p.requires_grad = False
-    crnn.eval()
-    for img_path in val_img_list:
-        img_tensor, gt = get_img(img_path)
-
-        img_tensor = Variable(img_tensor).to(device)
-
-        preds = crnn(img_tensor)
-        _, preds = preds.max(2)
-        preds = preds.transpose(1, 0).contiguous().view(-1)
-        preds_size = Variable(torch.IntTensor([preds.size(0)]))
-        raw_pred = converter.decode(preds.data, preds_size.data, raw=True)
-        sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
-
-        print('%-33s => %-33s, gt: %-20s, %s' % (raw_pred, sim_pred, gt, sim_pred == gt))
-        if sim_pred == gt:
-            test_acc += 1
-        #break
-
-    test_acc /= len(val_img_list)
-    avg_test_acc.append(test_acc)
-    avg_totalLoss /= trainloader.__len__()
-    totalLoss.append(avg_totalLoss)
-
-    info='epoch : %d , avg_totalLoss: %f , lr: %f  avg_test_acc: %f   avg_train_acc: %f ' % (epoch, avg_totalLoss, optimizer.param_groups[0]['lr'], test_acc, train_acc)
-    print(info)
-    # save result
-    if best_acc <= test_acc:
-        best_acc = test_acc
-        torch.save(crnn.cpu().state_dict(), args.model_save_path + 'cpu_model_parameter_' + str(epoch) + '.pkl')
-        crnn.to(device)
-
-        np.savetxt(args.model_save_path + 'total_loss.csv', totalLoss)
-        np.savetxt(args.model_save_path + 'avg_test_acc.csv', avg_test_acc)
-        np.savetxt(args.model_save_path + 'avg_train_acc.csv', avg_train_acc)
-
-    if test_acc == 1:
-        break
-    if(epoch % 50 == 0):
-        optimizer.param_groups[0]['lr'] = optimizer.param_groups[0]['lr'] * 0.6
+        b, c, h, w = conv.size()
+        assert h == 1, "the height of conv must be 1"
+        conv = conv.squeeze(2)
+        conv = conv.permute(2, 0, 1)  # [w, b, c]
+        conv = self.rnn(conv)
+        return conv
 
 
 
 
+if __name__ == '__main__':
+    model = GRCNN(37)
+    x = torch.rand(1, 1, 32, 100)
+    y = model(x)
+    # model = GRCL(32, 64, n_iter=3, kernel_size=3, stride=1, padding=1)
+    # x = torch.rand(1, 32, 32, 200)
+    # y = model(x)
+    #print(model)
