88b62357a2c41ffd6ca30ee0919b9ede3ef03f69
David Alami <david.alami@gmail.com>
2020-11-01 11:05:46 +0200

    Removed augmentation methods

--- dataloader.py
+++ dataloader.py
@@ -22,7 +22,7 @@ class MyDataSet(data.Dataset):
             # NOTE: 0 is reserved for 'blank' required by wrap_ctc
             self.alphabet_dict[self.alphabet[ii]] = ii + 1
 
-        print('find ',len(self.dataFileList),' images')
+        print('f ',len(self.dataFileList),' images')
 
         mean=[x/255 for x in [125.3,123.0,113.0]]
         std=[x/255 for x in [63.0,62.1,66.7]]
@@ -35,13 +35,7 @@ class MyDataSet(data.Dataset):
         try:
             imgPath=self.dataFileList[index]
             img = cv2.imread(imgPath)
-
-            if random.randint(0,10)>7:
-                img = self.addSaltNoise(img)
-            if random.randint(0,10)>7:
-                img = self.addBlurNoise(img)
-            img = self.random_crop(img)
-               
+            
             txt_name = os.path.basename(imgPath).replace('.jpg', '').split('_')[-1]
             txt_len = len(txt_name)
 
@@ -68,41 +62,6 @@ class MyDataSet(data.Dataset):
             print(e)
             return  self.__getitem__(index + 1)
         return img_tensor, txt_len, txt_label, txt_name
-
-    def random_crop(self, img):
-        height, width, _ = np.shape(img)
-        padding = 10
-        crop_img = np.zeros(shape=[height + 2*padding, width + 2*padding, 3], dtype=np.uint8)
-        crop_img[padding:padding+height, padding:padding+width, :] = img.copy()
-        x = np.random.randint(0,2*padding-1)
-        y = np.random.randint(0,2*padding-1)
-        img = crop_img[y:y+height, x:x+width, :]
-        return img
-
-    def addNoise(self,subImage):
-        subImage=self.addSaltNoise(subImage)
-        subImage=self.addBlurNoise(subImage)
-        return subImage
-
-    def addSaltNoise(self,subImage):
-        if np.random.randint(0,10)<8:
-            return subImage
-        height,width,_=np.shape(subImage)
-        rate=1.0*np.random.randint(0,10)/100.0
-        for jj in range(int(width*height*rate)):
-            row=np.random.randint(0,width-1)
-            col=np.random.randint(0,height-1)
-            value=np.random.randint(0,255)
-            subImage[col][row][:]=value
-        return subImage
-
-    def addBlurNoise(self,subImage):
-        if np.random.randint(0,10)<8:
-            return subImage
-        rand=np.random.randint(1,5)
-        subImage=cv2.blur(subImage,(rand,rand))
-        return subImage
-
 
 
 if __name__=='__main__':
